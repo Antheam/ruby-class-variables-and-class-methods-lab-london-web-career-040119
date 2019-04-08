59d9132c0afeedc6a58aad894ea324f61173e3cd
Anthea Merton <anthea.merton@gmail.com>
2019-04-08 08:36:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,14 +29,16 @@ def self.genres
 end
 
 def self.genre_count
-  genre_count ={}
-@@genres.each do |genre|
-  if genre_count[genre] += 1
-else
-  genre_count[genre]
-end
-end 
-genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
 
 def self.artist_count
